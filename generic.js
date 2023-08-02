0068facb9d81ba272e73dee0d0e916a3b2b503df
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-02 13:10:16 +0300

    Add a paragraph to DOM only with js

---
+++ generic.js
@@ -0,0 +1,5 @@
+body = document.getElementById('body');
+let paragraph = document.createElement('p');
+paragraph.style.color = 'red';
+paragraph.textContent = 'Hey I\'m red!';
+body.appendChild(paragraph);
