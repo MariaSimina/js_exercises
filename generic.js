b31ad3eb43494cc91ca9d89a6c0b2c49aa1b4c89
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-02 13:13:05 +0300

    Add a h3 element to DOM only with js

--- generic.js
+++ generic.js
@@ -1,5 +1,11 @@
 body = document.getElementById('body');
+
 let paragraph = document.createElement('p');
 paragraph.style.color = 'red';
 paragraph.textContent = 'Hey I\'m red!';
 body.appendChild(paragraph);
+
+let text = document.createElement('h3');
+text.style.color = 'blue';
+text.textContent = "I'm a blue h3!";
+body.appendChild(text);
