96eef2451181a72e31c40cb05a67c23123a8a70c
Maria Simina <maria.simina_groza@yahoo.com>
2023-08-02 13:22:44 +0300

    Add a div with elements to DOM only with js

--- generic.js
+++ generic.js
@@ -9,3 +9,12 @@ let text = document.createElement('h3');
 text.style.color = 'blue';
 text.textContent = "I'm a blue h3!";
 body.appendChild(text);
+
+let div = document.createElement('div');
+let title = document.createElement('h1');
+let para = document.createElement('p');
+title.textContent = "I'm in a div";
+para.textContent = "ME TOO!";
+div.style.cssText = "border: black solid; background: pink";
+div.append(title, para);
+body.appendChild(div);
